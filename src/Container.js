8fb1394c5098bebac2ff3a93c980bd8f15948da1
JP Driver <jpsdriver@gmail.com>
2017-02-23 17:07:38 +0000

    add Container component

---
+++ src/Container.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Container = ({ children, full, narrow, wide }) => {
+  let className = 'l-container'
+  if (full) {
+    className += ' l.container--full'
+  } else if (narrow) {
+    className += ' l.container--narrow'
+  } else if (wide) {
+    className += ' l.container--wide'
+  }
+  return <div className={className}>
+    {children}
+  </div>
+}
+
+export default Container
